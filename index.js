e201fa5364a1f1cff7fd6f465fd7ace18adce7a9
autumn-ragland <raglanda998@gmail.com>
2019-09-10 14:48:48 -0500

    Graded 09-10-2019 14:48:37

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
 // Print the first p tag to the console using the ID, class, and tag selectors.
 
 
-// let firstp = document.getElementsByTagName('p');
-// console.log(firstp[0]);
-// console.log(firstp.innerText);
+let firstp1 = document.getElementsByTagName('p');
+console.log(firstp1[0]);
+// !! : firstp1[0].innerText
+console.log(firstp1.innerText);
 
-// let firstp = document.getElementById('first');
-// console.log(firstp);
+let firstp2 = document.getElementById('first');
+// !! : firstp2.innerText
+console.log(firstp2);
 
-// let firstp = document.getElementsByClassName('special');
-// console.log(firstp);
+let firstp3 = document.getElementsByClassName('special');
+// !! : firstp3.innerText
+console.log(firstp3);
 
 
 
 // Print the special class using both the query selector and query all selector.
-//
-// let otherp = document.querySelector('p.special');
-// console.log(otherp.innerText);
 
-// let otherp = document.querySelectorAll('p.special');
-// for (i = 0; i < otherp.length; i++){
-//     console.log(otherp[i])
-// }
+let otherp1 = document.querySelector('p.special');
+console.log(otherp1.innerText);
+
+let otherp2 = document.querySelectorAll('p.special');
+for (i = 0; i < otherp2.length; i++){
+	// !! : otherp2[i].innerText
+    console.log(otherp2[i])
+}
 
 // Change the color of the h1 statement to blue.
 
-// let h1 = document.getElementsByTagName('h1');
-// h1[0].style.color = 'blue';
+let h1 = document.getElementsByTagName('h1');
+h1[0].style.color = 'blue';
 
 // Change the color of the last p tag to yellow.
 
