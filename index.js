835e72f32377e2f9d01df2b673f97e84f3184457
Dekevion Wade <Dekevionwade@yahoo.com>
2019-09-10 11:52:28 -0500

    answers

---
+++ index.js
@@ -0,0 +1,34 @@
+// Print the first p tag to the console using the ID, class, and tag selectors.
+
+
+// let firstp = document.getElementsByTagName('p');
+// console.log(firstp[0]);
+// console.log(firstp.innerText);
+
+// let firstp = document.getElementById('first');
+// console.log(firstp);
+
+// let firstp = document.getElementsByClassName('special');
+// console.log(firstp);
+
+
+
+// Print the special class using both the query selector and query all selector.
+//
+// let otherp = document.querySelector('p.special');
+// console.log(otherp.innerText);
+
+// let otherp = document.querySelectorAll('p.special');
+// for (i = 0; i < otherp.length; i++){
+//     console.log(otherp[i])
+// }
+
+// Change the color of the h1 statement to blue.
+
+// let h1 = document.getElementsByTagName('h1');
+// h1[0].style.color = 'blue';
+
+// Change the color of the last p tag to yellow.
+
+let lastp = document.getElementById('last');
+lastp.style.color = 'yellow';
